66d07d5e43c6556adcf745a7dd35b6b81a9a9346
進撃の雯雯老師 <askahu6@gmail.com>
2018-06-15 13:21:55 +0800

    Update scurve_CG.R

--- MROI/core/scurve_CG.R
+++ MROI/core/scurve_CG.R
@@ -3,27 +3,54 @@ rm(list=ls())
 library(data.table)
 library(dplyr)
 
-s_full <- function(b1,b2,b3,b4,b5,b6){
-  x <- (1:1000)/400
-  (b1 + b2 * exp(-1*(b3*x+b4)^b5)) * b6
-}
-
 s <- function(b){
-  x <- (1:1000)/400
-  cv <- 1-exp(-1*x^b)
+  # b <- 1.67; ret <- 0.75
+  x <- (1:3000)/1000
+  cv <- 1-exp(-1*(x)^b)
   mar <- diff(cv); roi <- cv/(1:length(cv))
-  plot.ts(mar,col=2); lines(roi)
+  # plot.ts(mar,col=2); lines(roi)
   lift <- roi[which(roi==max(roi))]/roi[which(mar==max(mar))]
   range <- which(roi==max(roi))/which(mar==max(mar))
   list(cv=cv,coef=c(b=b,lift=lift,range=range))
 }
-
 curvemap <- as.data.frame(
   t(sapply((500:2500)/1000,function(i){
     s(i)$coef
-  }))
-)
+})))
+rawcurve <- function(blift,brange){
+  # blift <- 3.4/3.1; brange <- 98/59 #1.096774, #1.661017
+  sel <- head(arrange(mutate(curvemap,l1=(1-lift/blift)^2,l2=(1-range/brange)^2),l1+l2),20)
+  sel <- (mutate(sel,l3=ifelse(l1<l2,l2,l1)) %>% arrange(l3))[1,1:3]
+  f <- function(x,ret=ret){1-exp(-1*(x)^sel$b)}
+  list(fomula=f,sel=sel)
+}
+getcurve <- function(f,C,Cs,ret=0.75){
+  # f <- rawcurve(3.4/3.1,98/59)
+  # C <- 98; Cs <- 0.34; ret <- 0.75
+  qx <- (1:3000)/1000
+  b <- f$sel$b
+  cv <- f$fomula(qx)/(qx)
+  prf2 <- qx[which(cv==max(cv))]/(C/(1-ret)) * (1-ret)
+  f <- function(x){(1-exp(-1*(x/(1-ret)*prf2)^b))}
+  cv <- (f(1:1000))/(1:1000)
+  prf <- Cs/cv[which(cv==max(cv))]
+  f <- function(x){prf * (1-exp(-1*(x/(1-ret)*prf2)^b))}
+  margin <- diff(f(1:1000))
+  roi <- f(1:1000)/(1:1000)
+  plot.ts(margin,col=2); lines(roi)
+  fomula <- paste0(prf,"*(1-exp(-1*x/(1-",ret,')*',prf2,")^",b,'))')
+  list(coef=c(prf=prf,prf2=prf2,b=b,ret=ret),fomula=fomula,fun=f)
+}
+scurve <- function(orange,rrange,ret){
+  o1 <- orange[1]
+  o2 <- orange[2]
+  r1 <- rrange[1]
+  r2 <- rrange[2]
+  f <- rawcurve(r2/r1,o2/o1)
+  getcurve(f,o2,r2,ret)
+}
 
-blift <- 3.4/3.1; brange <- 98/59
-sel <- head(arrange(mutate(curvemap,l1=(1-lift/blift)^2,l2=(1-range/brange)^2),l1+l2),20)
-mutate(sel,l3=ifelse(l1<l2,l2,l1)) %>% arrange(l3)
+scurve(c(59,98),c(0.31,0.34),0.75)
+
+
+
